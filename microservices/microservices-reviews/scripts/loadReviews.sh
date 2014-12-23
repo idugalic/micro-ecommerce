a98d689f226763538c03c6a2dfe857629ab3ff0c
idugalic <idugalic@westum.com>
2014-12-23 22:45:41 +0100

    #8 Implement HATEOAS on all services
    Manual controllers removed from 'review' service.
    API gateway has been changed accordingly.

--- microservices/microservices-reviews/scripts/loadReviews.sh
+++ microservices/microservices-reviews/scripts/loadReviews.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ROUTE=${ROUTE:-localhost:8081}
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"mstine","mlId":"1","title":"Toy Story (1995)","review":"Great movie!","rating":"5"}' -H "Content-Type: application/json"
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"mstine","mlId":"2","title":"GoldenEye (1995)","review":"Pretty good...","rating":"3"}' -H "Content-Type: application/json"
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"starbuxman","mlId":"2","title":"GoldenEye (1995)","review":"BOND BOND BOND!","rating":"5"}' -H "Content-Type: application/json"
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"starbuxman","mlId":"4","title":"Get Shorty (1995)","review":"Meh","rating":"3" }}' -H "Content-Type: application/json"
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"starbuxman","mlId":"5","title":"Copycat (1995)","review":"Nicely done!","rating":"4"}' -H "Content-Type: application/json"
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"littleidea","mlId":"2","title":"GoldenEye (1995)","review":"Good show!","rating":"4"}' -H "Content-Type: application/json"
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"littleidea","mlId":"3","title":"Four Rooms (1995)","review":"Could have been better...","rating":"3"}' -H "Content-Type: application/json"
-curl ${ROUTE}/reviews/reviews -X POST -d '{"userName":"littleidea","mlId":"5","title":"Copycat (1995)","review":"Nicely done!","rating":"4"}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"mstine","mlId":"1","title":"Toy Story (1995)","review":"Great movie!","rating":"5"}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"mstine","mlId":"2","title":"GoldenEye (1995)","review":"Pretty good...","rating":"3"}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"starbuxman","mlId":"2","title":"GoldenEye (1995)","review":"BOND BOND BOND!","rating":"5"}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"starbuxman","mlId":"4","title":"Get Shorty (1995)","review":"Meh","rating":"3" }}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"starbuxman","mlId":"5","title":"Copycat (1995)","review":"Nicely done!","rating":"4"}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"littleidea","mlId":"2","title":"GoldenEye (1995)","review":"Good show!","rating":"4"}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"littleidea","mlId":"3","title":"Four Rooms (1995)","review":"Could have been better...","rating":"3"}' -H "Content-Type: application/json"
+curl ${ROUTE}/reviews -X POST -d '{"userName":"littleidea","mlId":"5","title":"Copycat (1995)","review":"Nicely done!","rating":"4"}' -H "Content-Type: application/json"
--- microservices/microservices-reviews/src/main/java/io/pivotal/microservices/reviews/Application.java
+++ microservices/microservices-reviews/src/main/java/io/pivotal/microservices/reviews/Application.java
@@ -11,18 +11,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableDiscoveryClient
-@RestController
 public class Application extends RepositoryRestMvcConfiguration implements CommandLineRunner {
 
     @Autowired
@@ -42,20 +34,4 @@ public class Application extends RepositoryRestMvcConfiguration implements Comma
    		config.exposeIdsFor(Review.class);
    	}
 
-
-    @RequestMapping(value = "/reviews/reviews", method = RequestMethod.GET)
-    public Iterable<Review> reviews() {
-        return reviewRepository.findAll();
-    }
-
-    @RequestMapping(value = "/reviews/reviews/{mlId}", method = RequestMethod.GET)
-    public Iterable<Review> reviews(@PathVariable String mlId) {
-        return reviewRepository.findByMlId(mlId);
-    }
-
-    @RequestMapping(value = "/reviews/reviews", method = RequestMethod.POST)
-    public ResponseEntity<Review> createReview(@RequestBody Review review) {
-        reviewRepository.save(review);
-        return new ResponseEntity<>(review, HttpStatus.CREATED);
-    }
 }
