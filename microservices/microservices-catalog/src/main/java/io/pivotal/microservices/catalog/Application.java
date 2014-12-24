7048ff2983748a00d327db1222a2a3ce62c6a8ef
idugalic <idugalic@westum.com>
2014-12-24 17:59:48 +0100

    #8 Implement HATEOAS on all service
    Removed old controller on catalog service. HATEOAS services are used
    instead!!!

--- microservices/microservices-catalog/src/main/java/io/pivotal/microservices/catalog/Application.java
+++ microservices/microservices-catalog/src/main/java/io/pivotal/microservices/catalog/Application.java
@@ -2,25 +2,17 @@ package io.pivotal.microservices.catalog;
 
 import io.pivotal.microservices.catalog.models.Genre;
 import io.pivotal.microservices.catalog.models.Movie;
-import io.pivotal.microservices.catalog.repositories.GenreRepository;
-import io.pivotal.microservices.catalog.repositories.MovieRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableDiscoveryClient
-@RestController
 public class Application extends RepositoryRestMvcConfiguration {
 
 	@Override
@@ -30,31 +22,5 @@ public class Application extends RepositoryRestMvcConfiguration {
 	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Autowired
-    MovieRepository movieRepository;
-
-    @Autowired
-    GenreRepository genreRepository;
-
-    @RequestMapping(value = "/catalog/movies", method = RequestMethod.GET)
-    public Iterable<Movie> movies() {
-        return movieRepository.findAll();
-    }
-
-    @RequestMapping(value = "/catalog/movies/{mlId}", method = RequestMethod.GET)
-    public Movie movie(@PathVariable String mlId) {
-        return movieRepository.findByMlId(mlId);
-    }
-
-    @RequestMapping(value = "/catalog/genres", method = RequestMethod.GET)
-    public Iterable<Genre> genres() {
-        return genreRepository.findAll();
-    }
-
-    @RequestMapping(value = "/catalog/genres/{mlId}", method = RequestMethod.GET)
-    public Genre genre(@PathVariable String mlId) {
-        return genreRepository.findByMlId(mlId);
     }
 }
