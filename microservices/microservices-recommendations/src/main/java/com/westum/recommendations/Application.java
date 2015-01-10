0899431c1c3d12364833c058a9c617c3fe4ed721
idugalic <idugalic@westum.com>
2015-01-10 18:12:23 +0100

    #6: Implement basic oauth2 authorization server.

--- microservices/microservices-recommendations/src/main/java/com/westum/recommendations/Application.java
+++ microservices/microservices-recommendations/src/main/java/com/westum/recommendations/Application.java
@@ -6,8 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 import com.westum.recommendations.repositories.LikesRepository;
 import com.westum.recommendations.repositories.PersonRepository;
@@ -35,9 +39,34 @@ public class Application extends RepositoryRestMvcConfiguration implements Comma
         personRepository.deleteAll();
         likesRepository.deleteAll();
     }
-//    @Override
-//	protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-//		config.exposeIdsFor(Product.class, Likes.class, Person.class);
-//	}
+    @Configuration
+	@EnableOAuth2Resource
+	static class ResourceServer extends ResourceServerConfigurerAdapter {
+
+	  @Override
+	  public void configure(HttpSecurity http) throws Exception {
+	    http.requestMatchers().and().authorizeRequests()
+	     .antMatchers(HttpMethod.GET, "/recommendations/**").access("#oauth2.hasScope('read_recommendations') and (hasRole('ROLE_ADMIN') or hasRole('ROLE_USER'))")
+	     .antMatchers(HttpMethod.GET, "/products/**").access("#oauth2.hasScope('read_recommendations') and (hasRole('ROLE_ADMIN') or hasRole('ROLE_USER'))")
+	     .antMatchers(HttpMethod.POST, "/**").access("#oauth2.hasScope('write_recommendations') and hasRole('ROLE_ADMIN')")
+	     .antMatchers(HttpMethod.PUT, "/**").access("#oauth2.hasScope('write_recommendations') and hasRole('ROLE_ADMIN')")
+	     .antMatchers(HttpMethod.DELETE, "/**").access("#oauth2.hasScope('write_recommendations') and hasRole('ROLE_ADMIN')")
+	     
+//	     .antMatchers("/health/**").anonymous()
+//	     .antMatchers("/info/**").anonymous()
+	     .antMatchers("/metrics/**").access("#oauth2.hasScope('metrics')")
+         .antMatchers("/trace/**").access("#oauth2.hasScope('trace')")
+         .antMatchers("/dump/**").access("#oauth2.hasScope('dump')")
+         .antMatchers("/shutdown/**").access("#oauth2.hasScope('shutdown')")
+         .antMatchers("/beans/**").access("#oauth2.hasScope('beans')")
+         .antMatchers("/autoconfig/**").access("#oauth2.hasScope('autoconfig')")
+		 .antMatchers("/configprops/**").access("#oauth2.hasScope('configprops')")
+		 .antMatchers("/env/**").access("#oauth2.hasScope('env')")
+		 .antMatchers("/mappings/**").access("#oauth2.hasScope('mappings')");
+	     
+	  }
+
+	}
+
 
 }
--- microservices/microservices-reviews/src/main/java/com/westum/reviews/Application.java
+++ microservices/microservices-reviews/src/main/java/com/westum/reviews/Application.java
@@ -6,9 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 import com.westum.reviews.models.Review;
 import com.westum.reviews.repositories.ReviewRepository;
@@ -16,7 +20,6 @@ import com.westum.reviews.repositories.ReviewRepository;
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableDiscoveryClient
-@EnableOAuth2Resource
 public class Application extends RepositoryRestMvcConfiguration implements CommandLineRunner {
 
     @Autowired
@@ -35,5 +38,34 @@ public class Application extends RepositoryRestMvcConfiguration implements Comma
    	protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
    		config.exposeIdsFor(Review.class);
    	}
+    
+    @Configuration
+	@EnableOAuth2Resource
+	static class ResourceServer extends ResourceServerConfigurerAdapter {
+
+	  @Override
+	  public void configure(HttpSecurity http) throws Exception {
+	    http.requestMatchers().and().authorizeRequests()
+	     .antMatchers(HttpMethod.GET, "/reviews/**").access("#oauth2.hasScope('read_reviews') and (hasRole('ROLE_ADMIN') or hasRole('ROLE_USER'))")
+	     .antMatchers(HttpMethod.POST, "/**").access("#oauth2.hasScope('write_reviews') and hasRole('ROLE_ADMIN')")
+	     .antMatchers(HttpMethod.PUT, "/**").access("#oauth2.hasScope('write_reviews') and hasRole('ROLE_ADMIN')")
+	     .antMatchers(HttpMethod.DELETE, "/**").access("#oauth2.hasScope('write_reviews') and hasRole('ROLE_ADMIN')")
+	     
+//	     .antMatchers("/health/**").anonymous()
+//	     .antMatchers("/info/**").anonymous()
+	     .antMatchers("/metrics/**").access("#oauth2.hasScope('metrics')")
+         .antMatchers("/trace/**").access("#oauth2.hasScope('trace')")
+         .antMatchers("/dump/**").access("#oauth2.hasScope('dump')")
+         .antMatchers("/shutdown/**").access("#oauth2.hasScope('shutdown')")
+         .antMatchers("/beans/**").access("#oauth2.hasScope('beans')")
+         .antMatchers("/autoconfig/**").access("#oauth2.hasScope('autoconfig')")
+		 .antMatchers("/configprops/**").access("#oauth2.hasScope('configprops')")
+		 .antMatchers("/env/**").access("#oauth2.hasScope('env')")
+		 .antMatchers("/mappings/**").access("#oauth2.hasScope('mappings')");
+	     
+	  }
+
+	}
+
 
 }
