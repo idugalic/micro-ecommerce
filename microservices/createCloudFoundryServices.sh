d931efeca86386673579041059a81bcb82e6da67
idugalic <idugalic@westum.com>
2015-10-27 18:57:17 +0100

    Docs updated

--- microservices/createCloudFoundryServices.sh
+++ microservices/createCloudFoundryServices.sh
@@ -1,7 +1,4 @@
-cd microservices-config-server
+#!/bin/bash
 cf cups config-server -p '{"uri":"http://config-server-idugalic.cfapps.io"}'
-cd ..
-
-cd microservices-eureka
 cf cups eureka -p '{"uri":"http://eureka-idugalic.cfapps.io"}'
-cd ..
+cf cups recommendations-db -p '{"neo4jUri":"http://idugalicmicroservices.sb04.stations.graphenedb.com:24789/db/data/","neo4jUsername":"Idugalic_microservices","neo4jPassword":"mlzkAME4f85viYEwgSts"}'
--- microservices/microservices-recommendations/scripts/createGrapheneService.sh
+++ microservices/microservices-recommendations/scripts/createGrapheneService.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-URI="http://idugalicmicroservices.sb04.stations.graphenedb.com:24789"
-USERNAME="Idugalic_microservices"
-PASSWORD="mlzkAME4f85viYEwgSts"
 cf cups recommendations-db -p '{"neo4jUri":"http://idugalicmicroservices.sb04.stations.graphenedb.com:24789/db/data/","neo4jUsername":"Idugalic_microservices","neo4jPassword":"mlzkAME4f85viYEwgSts"}'
